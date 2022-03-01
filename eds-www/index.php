04a8f1cfbd889db1be2c1e2f7ef0574bab6bbdb8
Ethan-L-5115 <100323153+Ethan-L-5115@users.noreply.github.com>
2022-02-28 17:07:20 -0700

    Added to Style and Index

--- eds-www/index.php
+++ eds-www/index.php
@@ -9,25 +9,52 @@
     if ($connection->connect_error) {
         die("Connection Failure" . $connection->connect_error);
     }
+
+    $query = "SELECT * FROM status ORDER BY timestamp DESC LIMIT 10";
+    $result = $connection->query($query);
+
+    while ($row = $result->fetch_assoc()) {
+        
+        $id = $row['id']; 
+        $latitude = $row['latitude'];
+        $altitude = $row['altitude'];
+        $longitude = $row["longitude"];
+        $timestamp = $row['timestamp'];
+    }
 ?>
 
 <html>
-    <link href="style.css" rel="stylesheet">
     <head>
     <link rel="stylesheet" href="Style.css">
         <title>   
             Blue Horizon Weather Balloon Project
         </title>
-        <meta http-equiv="refresh" content="10">
-        <img src='https://militaryartsconnection.org/wp-content/uploads/2020/01/USAFA-logo.jpg' 
-            alt = 'Logo Goes Here' height = "206" width = "200" Align = 'middle' />
-    </head>
+        <!--<meta http-equiv="refresh" content="10"> -->
+        
+        </head>
+        
+        <div class="header">
+            <div class="image">
+                <image src="https://militaryartsconnection.org/wp-content/uploads/2020/01/USAFA-logo.jpg" 
+                    Alt="" height="200" width="200" Align="left"/>
+</div>  
+            <h1>
+                Blue Horizon Weather Balloon Project
+            </h1>
+           
+        </div>
+
+        
+
     <body>
-        <h1> 
-        <center>
-            Blue Horizon Weather Balloon Project
-        </center>
-        </h1>
+       
+        <h2 class="date">
+            
+            <?php
+                echo "Date: " . substr($timestamp, 0, 10) . "<br>";
+            ?>
+            
+        </h2>
 
        
     <div class="data">
@@ -46,21 +73,16 @@
                     $timestamp = $row['timestamp'];
                     
                     # echo "Full Data: " . $timestamp . "<br>";
-                    echo "Date: " . substr($timestamp, 0, 10) . " / " . "Time : " . substr($timestamp, 11) . "<br>"; 
+                    echo "Time : " . substr($timestamp, 11) . "<br>"; 
                     # echo "Time : " . substr($timestamp, 11) . "<br>";
                     echo "Altitude: " . $altitude . " " . "Latitude: " . $latitude . " " . "longitude: " . $longitude . "<br>";
-                    echo " " . "<br>";
+                    echo "-----------------------------------------------" . "<br>";
                 }   
                 $connection->close();
             ?>
-        <h2>
-            <?php
-                substr($timestamp, 0, 10)
-            ?>
-        </h2>
            
         </p1>
-            </div>
+    </div>
     
 <?php
 
