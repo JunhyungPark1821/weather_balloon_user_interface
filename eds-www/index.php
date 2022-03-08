7e03fd684da97fa9f213b23882df7b89b679398e
Ethan-L-5115 <100323153+Ethan-L-5115@users.noreply.github.com>
2022-03-08 13:27:11 -0700

    Indentation of Recent Data in CSS

--- eds-www/index.php
+++ eds-www/index.php
@@ -79,12 +79,12 @@
 
         <div class="recentData">  
             <h2> 
-                &emsp;&emsp;&emsp;Altitude  &emsp; Latitude  &emsp;   Longitude  &emsp; Temperature (Deg C)
+                Altitude  &emsp; Latitude  &emsp;   Longitude  &emsp; Temperature (Deg C)
             </h2>
 
             <h2>
                 <?php
-                    echo "&emsp;&emsp;&emsp;&emsp;&ensp;" . $altitude . "&ensp;&emsp;&emsp;&emsp;&emsp;" . $latitude . "&emsp;&emsp;&emsp;&emsp;&emsp;" . $longitude . "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;" . $temperature . "<br>"; 
+                    echo "&ensp;&ensp;&ensp;" . $altitude . "&ensp;&emsp;&emsp;&emsp;&emsp;" . $latitude . "&emsp;&emsp;&emsp;&emsp;&emsp;" . $longitude . "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;" . $temperature . "<br>"; 
                 ?>
             </h2>
         </div>   
